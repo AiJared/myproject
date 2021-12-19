9b53cd5b7ef4fda51a6f7416a1f27d0c9e42bca9
Kerriea-star <ojared511@gmail.com>
2021-12-19 19:43:58 +0300

    My django project

--- myapp/views.py
+++ myapp/views.py
@@ -4,16 +4,16 @@ from .models import Feature
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
  
- # Create your views here.
+# Create your views here.
 def index(request):
     features = Feature.objects.all()
     return render(request, 'index.html', {'features': features})
 def register(request):
     if request.method == 'POST':
-        username = request.POST('username')
-        email = request.POST('email')
-        password = request.method.POST('password')
-        password2 = request.method.POST('password2')
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
 
         if password == password2:
             if User.objects.filter(email = email).exists():
